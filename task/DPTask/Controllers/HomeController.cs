32e394938bec04f5355a11e95475bdfc2f7b895b
jovanamiskimovska <jovana.miskimovska@gmail.com>
2022-02-23 02:37:07 +0100

    .NET Core MVC

---
+++ task/DPTask/Controllers/HomeController.cs
@@ -0,0 +1,113 @@
+﻿using DPTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Grpc.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace DPTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+           _logger = logger;
+        }
+
+        //private IHostingEnvironment Environment;
+
+        //public HomeController(IHostingEnvironment _environment)
+        //{
+        //    Environment = _environment;
+        //}
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult Index(List<IFormFile> postedFiles)
+        //{
+        //    string wwwPath = this.Environment.WebRootPath;
+        //    string contentPath = this.Environment.ContentRootPath;
+
+        //    string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+        //    if (!Directory.Exists(path))
+        //    {
+        //        Directory.CreateDirectory(path);
+        //    }
+
+        //    List<string> uploadedFiles = new List<string>();
+        //    foreach (IFormFile postedFile in postedFiles)
+        //    {
+        //        string fileName = Path.GetFileName(postedFile.FileName);
+        //        using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+        //        {
+        //            postedFile.CopyTo(stream);
+        //            uploadedFiles.Add(fileName);
+        //            ViewBag.Message += string.Format("<b>{0}</b> uploaded.<br />", fileName);
+        //        }
+        //    }
+
+        //    return View();
+        //}
+
+        public IActionResult FileUpload()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult> FileUpload(IFormFile file)
+        {
+            await UploadFile(file);
+            TempData["msg"] = "File uploaded successfully";
+            return View();
+        }
+        public async Task<bool> UploadFile(IFormFile file)
+        {
+            string path = "";
+            bool isCopied = false;
+            try
+            {
+                if (file.Length > 0)
+                {
+                    string fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "Upload"));
+                    using (var filestream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(filestream);
+                    }
+                    isCopied = true;
+                }
+                else
+                {
+                    isCopied = false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return isCopied;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ task/DPTask/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DPTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
