52005be26fb83c500a09eabf75071a97f65bb687
jovanamiskimovska <jovana.miskimovska@gmail.com>
2022-03-06 23:07:07 +0100

    ReceiptScanner

---
+++ ReceiptScanner/Program.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Database;
+using Domain.Models;
+using Services.Implementations;
+using System;
+
+namespace ReceiptScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example Output");
+
+            Database productDatabase = new Database();
+            ProductService productService = new ProductService(productDatabase);
+
+            productService.PrintDomesticProducts();
+            productService.PrintImportedProducts();
+            productService.PrintProductsCost();
+            productService.PrintProductsCount();
+
+            Console.ReadLine();
+        }
+    }
+}
